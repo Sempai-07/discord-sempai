44fe27a6bf73c03a42c9b3de65bb9c5c05528c3d
Sempai-07 <118384270+Sempai-07@users.noreply.github.com>
2023-01-17 23:39:00 +0200

    Add files via upload

--- src/voice/leaveVoice.js
+++ src/voice/leaveVoice.js
@@ -1,6 +1,6 @@
-const leaceVoice = (connection = undefined) => {
+const leaveVoice = (connection = undefined) => {
   if (!connection) return console.log(new TypeError('Connection invalid'));
   else connection.destroy();
 };
 
-module.exports = leaceVoice;
+module.exports = leaveVoice;
