325ce9766f526f0dee43581c148359259d729098
Sempai-07 <118384270+Sempai-07@users.noreply.github.com>
2023-01-20 08:57:56 +0200

    Update
    Это ещё не всё

--- src/prototype/Message.js
+++ src/prototype/Message.js
@@ -1,9 +1,21 @@
 const { Client, Message } = require('discord.js');
 
-Message.prototype.randomReactionMessage = function(options) {
-  try {
-    this.react(options.emojis.cache.random()?.id)
-  } catch (e) {
-    console.log(new TypeError("Invalid Client"));
-  }
+Message.prototype.findMember = function(member, returnAuthor) {
+  const message = this;
+  
+  let u =
+    message.guild.members.cache.get(member) ||
+    message.guild.members.cache.find(
+      (m) =>
+        m.user.tag.toLowerCase() === member.toLowerCase() ||
+        m.user.username.toLowerCase() === member.toLowerCase() ||
+        m.displayName.toLowerCase() === member.toLowerCase()
+    ) || message.mentions.members.first() || message.guild.members.fetch(member).catch(e => u = message.member);
+    if (returnAuthor === true) return u.id === undefined ? message.member.id : u.id;
+    else return u.id;
 };
+
+Message.prototype.randomReactionMessage = function(client) {
+    const msg = this;
+    msg.react(client.emojis.cache.random()?.id)
+  }
