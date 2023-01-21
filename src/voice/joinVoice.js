33a67c14cfee91653c533f4a2c238d9f6798baf3
Sempai-07 <118384270+Sempai-07@users.noreply.github.com>
2023-01-21 12:23:47 +0200

    Add files via upload

--- src/voice/joinVoice.js
+++ src/voice/joinVoice.js
@@ -1,8 +1,8 @@
 const { joinVoiceChannel } = require('@discordjs/voice');
 
 const joinVoice = (msg = undefined, voiceid = undefined) => {
-    if (this.msg === undefined) return console.log(new TypeError('Message not specified'));
-    else if (this.voiceid === undefined) return console.log(new TypeError('VoiceId not specified'));
+    if (msg === undefined) return console.log(new TypeError('Message/Interaction not specified'));
+    voiceid = voiceid === undefined ? member.voice?.channelId : voiceid
     const channel = msg.guild.channels.cache.get(voiceid);
     const connection = joinVoiceChannel({
       channelId: channel.id,
