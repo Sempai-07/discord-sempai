45b994e8dd95d8a5a4b8cbde385238f286d73f93
Sempai-07 <118384270+Sempai-07@users.noreply.github.com>
2023-01-28 13:07:22 +0200

    Delete src/prototype directory

--- src/prototype/Member.js
+++
@@ -1,7 +0,0 @@
-const { GuildMember, Guild } = require('discord.js');
-
-Object.defineProperty(GuildMember.prototype, "perms", {
-    get: function perms() {
-      return this.permissions.toArray();
-    }
-});
