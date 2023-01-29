ffaa61e9fa17a5bd80aa530e60546d98a3a20a64
Sempai-07 <118384270+Sempai-07@users.noreply.github.com>
2023-01-29 17:59:49 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// Default Classec
+// Classec
 exports.Bot = require("./structures/Bot.js");
 exports.Database = require("./structures/Database.js");
 exports.MessageEmbed = require("./structures/MessageEmbed.js");
@@ -6,13 +6,46 @@ exports.ActionComponent = require("./structures/ActionComponent.js");
 exports.ModalText = require("./structures/ModalText.js");
 exports.Modal = require("./structures/Modal.js");
 exports.MessageAttachment = require("./structures/MessageAttachment.js");
-exports.Parser = require("./util/Parser.js");
+exports.Parser = require("./structures/Parser.js");
+exports.Music = require("./structures/Music.js");
+exports.Link = require("./util/Link.js");
+exports.Invite = require("./util/Invite.js");
+exports.Role = require("./util/Role.js");
 exports.Util = require("./util/Util.js");
 
-// Voice Function/Classec
+// Function
+exports.encryption = require("./function/util/encryption.js");
+exports.decoding = require("./function/util/decoding.js");
+exports.isPermissions = require("./function/util/isPermissions.js");
+exports.findMember = require("./function/util/findMember.js");
+exports.findMembers = require("./function/util/findMembers.js");
+exports.findChannel = require("./function/util/findChannel.js");
 exports.joinVoice = require('./voice/joinVoice.js');
 exports.leaveVoice = require('./voice/leaveVoice.js');
 
-// Function
-exports.encryption = require("./function/encryption.js");
-exports.decoding = require("./function/decoding.js");
+// Util classes
+exports.Collection = require("./djs/classec.js").Collection;
+exports.WebhookClient = require("./djs/classec.js").WebhookClient;
+exports.GuildMember = require("./djs/classec.js").GuildMember;
+exports.Message = require("./djs/classec.js").Message;
+exports.Guild = require("./djs/classec.js").Guild;
+exports.Client = require("./djs/classec.js").Client;
+
+// Util djs
+exports.ApplicationCommandType = require("./djs/util.js").ApplicationCommandType;
+exports.PermissionsBitField = require("./djs/util.js").PermissionsBitField;
+exports.ActivityType = require("./djs/util.js").ActivityType;
+exports.ComponentType = require("./djs/util.js").ComponentType;
+exports.Events = require("./djs/util.js").Events;
+exports.GatewayIntentBits = require("./djs/util.js").GatewayIntentBits;
+exports.PermissionFlagsBits = require("./djs/util.js").PermissionFlagsBits;
+exports.TextInputStyle = require("./djs/util.js").TextInputStyle;
+exports.ButtonStyle = require("./djs/util.js").ButtonStyle;
+exports.ChannelType = require("./djs/util.js").ChannelType;
+exports.Partials = require("./djs/util.js").Partials;
+exports.RESTJSONErrorCodes = require("./djs/util.js").RESTJSONErrorCodes;
+exports.AuditLogEvent = require("./djs/util.js").AuditLogEvent;
+exports.DataResolver = require("./djs/util.js").DataResolver;
+exports.MessageActivityType = require("./djs/util.js").MessageActivityType;
+exports.CommandInteraction = require("./djs/util.js").CommandInteraction;
+exports.MessagePayload = require("./djs/util.js").MessagePayload;
