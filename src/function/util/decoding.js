c24714bd737d160216855b50988e85478fb0c663
Sempai-07 <118384270+Sempai-07@users.noreply.github.com>
2023-01-30 07:01:09 +0200

    Create decoding.js

---
+++ src/function/util/decoding.js
@@ -0,0 +1,12 @@
+const crypto = require('crypto');
+
+const decoding = (message, key) => {
+  let userKey = crypto.createDecipher('aes-128-cbc', key);
+  let text = userKey.update(message, 'hex', 'utf8');
+  text += userKey.final('utf8');
+  return text;
+};
+    
+module.exports = decoding;
+
+// © 2022 @Sempai Development
