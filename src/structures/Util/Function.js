d516f5f6442b9a5013849decf889d907df681af7
Sempai-07 <118384270+Sempai-07@users.noreply.github.com>
2023-02-23 11:42:35 +0200

    Add files via upload

--- src/structures/Util/Function.js
+++ src/structures/Util/Function.js
@@ -1,74 +1,82 @@
 const EmbedParser = (msg) => {
   const embeds = [];
-  let newEmbed = msg.split("{newEmbed:").slice(1);
-  for (let parserEmbed of newEmbed) {
-    parserEmbed = parserEmbed.slice(0, parserEmbed.length - 1);
+  const newEmbeds = msg.split("{newEmbed:").slice(1);
+  for (let parserEmbed of newEmbeds) {
+    parserEmbed = parserEmbed.slice(0, -1);
     const embed = {};
     const checkContent = (content) => parserEmbed.includes(`{${content}:`);
-    
-    if (checkContent("author")) {
+
+    if (checkContent("{author")) {
       const author = parserEmbed.split("{author:")[1].split("}")[0].split(":");
       embed.author = {
         name: author.shift()?.trim() || "",
-        iconURL: author.join(":")?.trim() || "",
+        iconURL: author.join(":").trim() || "",
       }
     };
-    
-    if (checkContent("thumbnail")) {
+
+    if (checkContent("{thumbnail")) {
       embed.thumbnail = parserEmbed.split("{thumbnail:")[1].split("}")[0].trim();
     }
-    
-    if (checkContent("authorURL")) {
-      if (!embed.author) return console.log(new TypeError("{author:} was not used"));
+
+    if (checkContent("{authorURL")) {
+      if (!embed.author) throw new TypeError("{author:} was not used");
       embed.author.url = parserEmbed.split("{authorURL:")[1].split("}")[0].trim();
     }
-    
-    if (checkContent("url")) {
-      if (!embed.title) return console.log(new TypeError("{title:} was not used"));
+
+    if (checkContent("{url")) {
+      if (!embed.title) throw new TypeError("{title:} was not used");
       embed.url = parserEmbed.split("{url:")[1].split("}")[0].trim();
     }
-    
-    if (checkContent("title")) {
+
+    if (checkContent("{title")) {
       const title = parserEmbed.split("{title:")[1].split("}")[0].split(":");
       embed.title = title.shift()?.trim() || ""
     }
-    
-    if (checkContent("description")) {
+
+    if (checkContent("{description")) {
       const description = parserEmbed.split("{description:")[1].split("}");
       embed.description = description.shift()?.trim() || ""
     }
-    
-    if (checkContent("color")) {
+
+    if (checkContent("{color")) {
       const color = parserEmbed.split("{color:")[1].split("}")[0].split(":");
       embed.color = color.shift()?.trim() || ""
     }
-    
-    if (checkContent("footer")) {
+
+    if (checkContent("{footer")) {
       const footer = parserEmbed.split("{footer:")[1].split("}")[0].split(":");
       embed.footer = {
         text: footer.shift()?.trim() || "",
-        iconURL: footer.join(":")?.trim() || ""
+        iconURL: footer.join(":").trim() || ""
       };
     }
-    
+
     if(parserEmbed.includes("{timestamp")) {
       let t = parserEmbed.split("{timestamp")[1].split("}")[0].replace(":", "").trim();
-      if (t === "" || t === "ms") {
-        t = Date.now();
-      }
-      embed.timestamp = new Date(t);
+      embed.timestamp = t === "" || t === "ms" ? new Date() : new Date(parseInt(t));
     }
-    
-    if (checkContent("image")) {
+
+    if (checkContent("{image")) {
       embed.image = parserEmbed.split("{image:")[1].split("}")[0].trim()
     }
+
+    if (checkContent("{field")) {
+      const fields = parserEmbed.split("{field:");
+      embed.fields = fields.slice(1).map(field => {
+        const [name, value, inline = false] = field.split("}")[0].trim().split(":");
+        return {
+          name: name || "\u200B",
+          value: value || "\u200B",
+          inline: inline === "true",
+        };
+      });
+    }
+
     embeds.push(embed);
   }
-  return embeds[0]
+  return embeds[0];
 };
 
 module.exports = {
-  EmbedParser: EmbedParser
-}
-
-// © 2022 @Sempai Development
+  EmbedParser
+};
