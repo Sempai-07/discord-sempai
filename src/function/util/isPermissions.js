0b6b9933f94edcf529b5de62e537b6b49cc843f3
Sempai-07 <118384270+Sempai-07@users.noreply.github.com>
2023-03-04 16:33:26 +0200

    Delete src/function/util directory

--- src/function/util/isPermissions.js
+++
@@ -1,8 +0,0 @@
-const isPermissions = (message, perms) => {
-  perms = Array.isArray(perms) === true ? perms : [perms];
-  return message.member?.permissions?.has(perms);
-};
-
-module.exports = isPermissions;
-
-// © 2022 @Sempai Development
